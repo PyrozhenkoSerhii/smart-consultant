49804616c53b555a6157af5e8c8ca1a771883921
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2018-05-20 14:26:48 +0300

    socket added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect(config.connectionString, (err) => {
         if (err) console.log('Database error' + err);
         else {
             console.log('Connected to database ' + config.dbName);
-            chatModule.socketCreation();
+            // chatModule.socketCreation();
         }
     }
 );
@@ -52,9 +52,105 @@ let http = require('http').Server(app);
 http.listen(port, () => {
     console.log('Main server app listening on port ' + port);
 });
-let io = require('socket.io')(http);
+let serverSocket = require('socket.io')(http);
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+serverSocket.on('connection', (clientSocket) => {
+    console.log('socket connection established');
+
+    clientSocket.on('room', (req) => {
+        clientSocket.join(req.data.room);
+
+        Chat.getAllByRoom(req.data.room, (err, messages) => {
+            if (err) {
+                console.log(err);
+            } else {
+                serverSocket.to(req.data.room).emit('output', messages);
+
+                if (req.data.type === 'customer') {
+                    let serverMessage = new Chat({
+                        user: 'system',
+                        message: req.data.user + ' joined to the room',
+                        profileImg: null,
+                        room: req.data.room,
+                        date: Date.now(),
+                        type: 'private',
+                        isSystemMessage: true
+                    });
+
+                    Chat.addPrivateMessage(serverMessage, (err,res)=>{
+                        if(err) console.log(err);
+                        else{
+                            Chat.getAllByRoom(req.data.room, (err, res) => {
+                                if (err) console.log(err);
+                                serverSocket.to(req.data.room).emit('output', res);
+                                serverSocket.to(req.data.room).emit('customerConnected', req.data.time);
+                            })
+                        }
+                    });
+                }
+            }
+        });
+    });
+
+    clientSocket.on('input', (data) => {
+        let newChat = new Chat(data.chat);
+
+        Chat.addPrivateMessage(newChat, (err, savedMessage) => {
+            if (err) {
+                console.log(err);
+            } else {
+                Chat.getAllByRoom(data.chat.room, (err, res) => {
+                    if (err) console.log(err);
+                    serverSocket.to(data.chat.room).emit('output', res);
+                });
+
+                console.log('time' + data.time);
+                Chat.getAllByRoomAndTime(data.chat.room, data.time, (err,res)=>{
+                    if(err)console.log(err);
+                    serverSocket.to(data.chat.room).emit('customerOutput', res);
+                })
+            }
+        });
+    });
+
+    clientSocket.on('disconnectedMessage', (data)=>{
+
+        let serverMessage = new Chat({
+            user: 'system',
+            message: data.user + ' left the room',
+            profileImg: null,
+            room: data.room,
+            date: Date.now(),
+            type: 'private',
+            isSystemMessage: true
+        });
+
+        Chat.addPrivateMessage(serverMessage, (err)=>{
+            if(err) console.log(err);
+            else{
+                Chat.getAllByRoom(data.room, (err, res) => {
+                    if (err) console.log(err);
+                    serverSocket.to(data.room).emit('output', res);
+                    serverSocket.to(data.room).emit('customerDisconnected', res);
+                })
+            }
+        });
+    });
+
+    clientSocket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    });
+
+    clientSocket.on('forum', ()=>{
+        Chat.getAllByRoom()
+    });
+
+    // hellman
+    clientSocket.on('hellmanInitializeCustomerData', (data) => {
+        serverSocket.to(data.room).emit('receiveFromCustomer', data);
+    });
+    clientSocket.on('hellmanInitializeConsultantData', (data)=>{
+        serverSocket.to(data.room).emit('receiveFromConsultant', data);
+    });
 });
 
